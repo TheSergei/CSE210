f2f7406e8a7091204d1f556849188fe00769d4cd
TheSergei <squintanilla@byupathway.edu>
2025-03-15 09:20:21 -0500

    encapsulation

--- week03/Fractions/Program.cs
+++ week03/Fractions/Program.cs
@@ -4,6 +4,12 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Fractions Project.");
+        Fraction f1 = new Fraction();
+        System.Console.WriteLine(f1.GetFractionString());
+        System.Console.WriteLine(f1.GetDecimalValue());
+
+        Fraction f2 = new Fraction();
+        System.Console.WriteLine(f2.GetFractionString());
+        System.Console.WriteLine(f2.GetDecimalValue());
     }
 }
